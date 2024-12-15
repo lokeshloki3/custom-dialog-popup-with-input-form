9b71491b3a0041552a24f114aecff90c4c55204d
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-15 18:10:40 +0530

    reset added

--- src/components/DialogPopup.jsx
+++ src/components/DialogPopup.jsx
@@ -30,6 +30,15 @@ const DialogPopup = ({
     onClose();
   };
 
+  const resetForm = () => {
+    setInputs((prevInputs) =>
+      Object.keys(prevInputs).reduce((acc, key) => {
+        acc[key] = prevInputs[key] === "" ? inputFields.find((field) => field.id === key)?.value || "" : prevInputs[key];
+        return acc;
+      }, {})
+    );
+  };
+
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96">
@@ -64,16 +73,22 @@ const DialogPopup = ({
         ))}
         <div className="flex justify-end space-x-2">
           <button
-            onClick={onClose}
-            className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"
-          >
-            Cancel
-          </button>
-          <button
             onClick={handleSubmit}
             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
           >
             {buttonText}
+          </button>
+          <button
+            onClick={resetForm}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Reset
+          </button>
+          <button
+            onClick={onClose}
+            className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"
+          >
+            Cancel
           </button>
         </div>
       </div>
