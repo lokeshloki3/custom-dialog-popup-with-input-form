579d750921e259bc84168207d0dada21efd79d2a
lokeshloki3 <lokesh1992p@gmail.com>
2024-12-16 22:58:24 +0530

    ui updated

--- src/components/DialogPopup.jsx
+++ src/components/DialogPopup.jsx
@@ -84,7 +84,12 @@ const DialogPopup = ({
           >
             Reset
           </button>
-
+          <button
+            onClick={onClose}
+            className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"
+          >
+            Cancel
+          </button>
         </div>
       </div>
     </div>
